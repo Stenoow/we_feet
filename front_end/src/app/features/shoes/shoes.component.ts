8e12445fbd8cf87a6608b0dd99f462db08b65056
Stenoow <louloubomont@hotmail.fr>
2024-10-13 22:03:32 +0200

    Add style to all steps

--- front_end/src/app/features/shoes/shoes.component.ts
+++ front_end/src/app/features/shoes/shoes.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {SearchDataService} from '../../shared/pipes/search-data.service';
 
 @Component({
   selector: 'app-shoes',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './shoes.component.html',
   styleUrl: './shoes.component.css'
 })
 export class ShoesComponent {
+  constructor(private formDataService: SearchDataService) { }
 
+  ngOnInit() {
+    const savedData = this.formDataService.getAllData();
+    for (let i = 0; savedData.length; i++) {
+      console.log(savedData[i])
+    }
+    console.log(savedData)
+    console.log(savedData.length)
+  }
 }
--- front_end/src/app/shared/pipes/search-data.service.ts
+++ front_end/src/app/shared/pipes/search-data.service.ts
@@ -7,7 +7,10 @@ export class SearchDataService {
   private formData: any = {
     step1: {},
     step2: {},
-    step3: {}
+    step3: {},
+    step4: {},
+    step5: {},
+    step6: {}
   };
 
   constructor() { }
