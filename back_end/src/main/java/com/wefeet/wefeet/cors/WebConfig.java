880f941f0044a66c27a4addf57b972ddd7b3de96
Stenoow <louloubomont@hotmail.fr>
2024-10-15 17:51:56 +0200

    Fix WebConfig

--- back_end/src/main/java/com/wefeet/wefeet/cors/WebConfig.java
+++ back_end/src/main/java/com/wefeet/wefeet/cors/WebConfig.java
@@ -1,5 +1,6 @@
 package com.wefeet.wefeet.cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowedOrigin}")
+    private String allowedOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Permettre CORS sur les routes commençant par /api
-                .allowedOrigins("http://localhost:4200")  // Autoriser Angular en développement
+                .allowedOrigins(allowedOrigin)  // Autoriser Angular en développement
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Méthodes HTTP autorisées
                 .allowedHeaders("*")  // Autoriser tous les en-têtes
                 .allowCredentials(true);  // Autoriser les cookies si nécessaire
